8c603c55a24d72160340cfa9b9ad514d87e662e1
Miguel Rios <miguelriosoliveira@gmail.com>
2021-03-19 15:59:03 -0300

    Trying to find the error in SendGrid

--- src/sendgrid.js
+++ src/sendgrid.js
@@ -1,17 +1,6 @@
 require("dotenv/config");
 const axios = require("axios").default;
 
-let options = {
-  method: "POST",
-  url: "https://api.sendgrid.com/v3/validations/email",
-  headers: {
-    "content-type": "application/json",
-    authorization: "Bearer <<YOUR_API_KEY_HERE>>",
-  },
-  body: { email: "example@example.com", source: "signup" },
-  json: true,
-};
-
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
 axios
@@ -31,18 +20,15 @@ axios
   .then((response) => console.log("DEU BOM", response))
   .catch((error) => {
     if (error.response) {
-      console.log("tem error.response");
       // Request made and server responded
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
+      console.log("error.response.data", error.response.data);
+      console.log("error.response.status", error.response.status);
+      console.log("error.response.headers", error.response.headers);
     } else if (error.request) {
-      console.log("tem error.request");
       // The request was made but no response was received
-      console.log(error.request);
+      console.log("error.request", error.request);
     } else {
-      console.log("tem else");
       // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
+      console.log("error.message", error.message);
     }
   });
